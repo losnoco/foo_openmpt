d7e80d6421dfcebbc853b40cea744cf8ce72c76a
Christopher Snowhill <kode54@gmail.com>
2019-04-08 18:01:47 -0700

    - Updated libopenmpt to version 0.4.4 with bug fixes - Version is now 0.4.4

--- foo_openmpt.cpp
+++ foo_openmpt.cpp
@@ -4,6 +4,10 @@
 /*
 	change log
 
+2019-04-09 00:58 UTC - kode54
+- Updated libopenmpt to version 0.4.4 with bug fixes
+- Version is now 0.4.4
+
 2019-02-11 09:20 UTC - kode54
 - Updated libopenmpt to version 0.4.3 with bug and security fixes
 - Version is now 0.4.3
